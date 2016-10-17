21e65aff625847a51bddfdd23cb66299256fee31
Peter Souter <peter.souter+GIT@puppet.com>
2016-10-17 15:51:08 -0700

    Remove NTP management

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,7 +38,7 @@ inherits ::cis_rhel7::params
   include ::cis_rhel7::rule::rule_3_1
   include ::cis_rhel7::rule::rule_3_2
   include ::cis_rhel7::rule::rule_3_3
-  include ::cis_rhel7::rule::rule_3_6
+  # include ::cis_rhel7::rule::rule_3_6
   include ::cis_rhel7::rule::rule_3_7
   include ::cis_rhel7::rule::rule_3_8
   include ::cis_rhel7::rule::rule_3_9
